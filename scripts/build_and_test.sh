332ebe6bc6c9558f2c1ed06e476965b29b511647
Tony Redekop <tony.redekop@outlook.com>
2024-03-24 21:24:37 -0700

    added comments in build/test script

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-# Script is used for building and testing on a local machine and using a CI/CD platform 
+# Script is used for building and testing in  local environments
+# and on VMs/containers/runners provided by CI/CD platform, decoupling 
+# the CI pipeline from the CI/CD platform. 
+# This ensure congruence in the CI sequence between local and test 
+# environments and makes it easier to migrate to a different CI/CD platform.
 
-cargo build --verbose             # builds (we keep this here 
-cargo test --verbose              # builds and tests
+cargo build --verbose             # `cargo build` used here to be explicit & for separate reporting
+cargo test --verbose              # `cargo test` builds project and runs unit / integration tests
 cargo tarpaulin --ignore-tests    # code coverage
 cargo clippy -- -D warnings
 cargo fmt -- --check  
